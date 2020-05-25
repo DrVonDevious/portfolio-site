ea842b933acb00d4b19a92f3e401671f2cc4d399
DrVonDevious <drvondevious@gmail.com>
2020-05-25 14:36:59 -0500

    Add profile image

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../css/ProfileImage.css'
 
 const ProfileImage = () => {
   return (
-    <div>
+    <div className="image-container">
+      <img className="profile-image" src={process.env.PUBLIC_URL + '/profile-image.jpg'} alt="Profile Image" />
     </div>
   )
 }
--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -4,18 +4,20 @@ import ProfileImage from '../components/ProfileImage.js'
 import About from '../components/About.js'
 import Skill from '../components/Skill.js'
 
+import '../css/AboutContainer.css'
+
 const AboutContainer = () => {
   return (
-    <div>
-      <ProfileImage />
-      <About />
+    <div className="about-container">
+      <ProfileImage className="profile-image"/>
+      <About className="about"/>
       <div>
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill className="skill-1"/>
+        <Skill className="skill-2"/>
+        <Skill className="skill-3"/>
+        <Skill className="skill-4"/>
+        <Skill className="skill-5"/>
+        <Skill className="skill-6"/>
       </div>
     </div>
   )
